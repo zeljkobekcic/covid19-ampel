4da44581a82879c8fb18634ac0601b7417f0ded2
Zeljko Bekcic <bekciczeljko@gmail.com>
2020-03-22 17:33:25 +0100

    Adding secret key

--- covid19ampel/__init__.py
+++ covid19ampel/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.secret_key = b'some really random key'
 CORS(app)
 
 from . import routes
--- covid19ampel/aggregator.py
+++ covid19ampel/aggregator.py
@@ -1,6 +1,4 @@
 import json
-import os
-import psycopg2
 
 import numpy as np
 import pandas as pd
